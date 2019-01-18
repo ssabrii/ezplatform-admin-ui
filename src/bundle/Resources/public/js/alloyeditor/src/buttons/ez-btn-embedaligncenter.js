662ee8b72d5b12f01b643f30d231d47fbda63541
Dariusz Szut <dew326@gmail.com>
2019-01-18 13:37:39 +0100

    added entries

--- src/bundle/Resources/public/js/alloyeditor/src/buttons/ez-btn-embedaligncenter.js
+++ src/bundle/Resources/public/js/alloyeditor/src/buttons/ez-btn-embedaligncenter.js
@@ -4,16 +4,17 @@ import AlloyEditor from 'alloyeditor';
 import EzEmbedAlign from '../base/ez-embedalign';
 
 export default class EzEmbedAlignCenter extends EzEmbedAlign {
-    static get key() {
-        return 'ezembedcenter';
-    }
+	static get key() {
+		return 'ezembedcenter';
+	}
 }
 
 AlloyEditor.Buttons[EzEmbedAlignCenter.key] = AlloyEditor.EzEmbedAlignCenter = EzEmbedAlignCenter;
+eZ.addConfig('ezAlloyEditor.ezEmbedAlignCenter', EzEmbedAlignCenter);
 
 EzEmbedAlignCenter.defaultProps = {
-    alignment: 'center',
-    iconName: 'image-center',
-    cssClassSuffix: 'embed-center',
-    label: Translator.trans(/*@Desc("Center")*/ 'embed_align_center_btn.label', {}, 'alloy_editor'),
+	alignment: 'center',
+	iconName: 'image-center',
+	cssClassSuffix: 'embed-center',
+	label: Translator.trans(/*@Desc("Center")*/ 'embed_align_center_btn.label', {}, 'alloy_editor'),
 };
