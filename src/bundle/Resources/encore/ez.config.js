c94d85e8bb9ea3be80610a2db777e13d604266d8
Dariusz Szut <dew326@gmail.com>
2019-01-22 12:24:06 +0100

    added modules

--- src/bundle/Resources/encore/ez.config.js
+++ src/bundle/Resources/encore/ez.config.js
@@ -1,15 +1,8 @@
 const path = require('path');
-const fs = require('fs');
 const addJSEntries = require('./ez.js.config.js');
 const addCSSEntries = require('./ez.css.config.js');
 
 module.exports = (Encore) => {
     addJSEntries(Encore);
     addCSSEntries(Encore);
-
-    const config = Encore.getWebpackConfig();
-
-    config.name = 'ez-admin-ui';
-
-    return config;
 };
