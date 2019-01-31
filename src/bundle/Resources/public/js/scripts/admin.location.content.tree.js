edf2e55fc2ffe1956a5869c3089f33e3522bdbff
Piotr Nalepa <piotr.nalepa@ez.no>
2019-01-31 15:48:16 +0100

    EZP-30090: As an editor I would like content tree panel staying opened until clicking on a Content Tree button again

--- src/bundle/Resources/public/js/scripts/admin.location.content.tree.js
+++ src/bundle/Resources/public/js/scripts/admin.location.content.tree.js
@@ -1,5 +1,9 @@
 (function(doc, React, ReactDOM, eZ) {
     const contentTreeContainer = doc.querySelector('.ez-content-tree-container');
+    const btn = doc.querySelector('.btn---show-content-tree');
+    const toggleContentTreePanel = () => {};
+
+    btn.addEventListener('click', toggleContentTreePanel, false);
 
     ReactDOM.render(React.createElement(eZ.modules.ContentTree, {}), contentTreeContainer);
 })(window.document, window.React, window.ReactDOM, window.eZ);
